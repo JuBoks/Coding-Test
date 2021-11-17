ffde9962216200ada9926cad9dff48ec105c2ca6
JuBoks <89145094+JuBoks@users.noreply.github.com>
2021-11-17 15:56:45 +0900

    문자열재정렬

--- 구현/BOJ_18406_럭키스트레이트.py
+++ 구현/BOJ_18406_럭키스트레이트.py
@@ -1,1 +1,15 @@
-print('hi')
+N = list(input())
+length = len(N) // 2
+left = N[:length]
+right = N[length:]
+
+sum = 0
+for i in left:
+    sum += int(i)
+for i in right:
+    sum -= int(i)
+
+if sum == 0:
+    print('LUCKY')
+else:
+    print('READY')
---
+++ 구현/이코테_문자열재정렬.py
@@ -0,0 +1,21 @@
+N = input()
+sum = 0
+newStr = []
+
+# 1. 아스키코드 값을 통해 숫자인지 알파벳인지 구분
+# for i in N:
+#     if ord(i) >= 48 and ord(i) <= 57:
+#         sum += int(i)
+#     else:
+#         newStr.append(i)
+
+# 2. 💖isalpha() 함수를통해 알파벳을 구분💖
+for i in N:
+    if i.isalpha():
+        newStr.append(i)
+    else:
+        sum += int(i)
+
+newStr.sort()
+newStr = ''.join(newStr)
+print(newStr + str(sum))
